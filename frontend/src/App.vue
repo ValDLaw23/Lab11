de0d6681a511f61e81419b00ca5c5604c3995c2a
Valeria Espinoza <76666534+ValDLaw@users.noreply.github.com>
2023-06-05 20:15:16 -0500

    Logo + remove

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,24 +13,84 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+.title-box {
+  background-image: url('@/assets/logo_utec.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
 }
 
-nav {
-  padding: 30px;
+.links {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 660px;
+  margin-right: 660px;
+  height: 20px;
+  border-radius: 8px;
+  background-color: #fffb00;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
-nav a {
+.router-link {
+  margin: 100px;
+}
+
+.router-link-active {
   font-weight: bold;
-  color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.router-link-exact-active {
+  color: #4caf50;
 }
+
+.router-view {
+  margin-top: 20px;
+  padding: 20px;
+  background-color: #9103f6;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: opacity 0.3s ease;
+}
+
+.fadeIn {
+  opacity: 0;
+  transform: translateY(-20px);
+  transition: opacity 1s ease, transform 1s ease;
+}
+
+.fadeIn.show {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.router-view-enter-active,
+.router-view-leave-active {
+  opacity: 0;
+}
+
+.router-view-enter,
+.router-view-leave-to {
+  opacity: 1;
+}
+
+.app {
+  height: 1000px;
+  background-color: rgb(0, 179, 255);
+}
+
+@keyframes bannerAnimation {
+  0% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(10px);
+  }
+}
+
 </style>
