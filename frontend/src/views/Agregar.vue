ef907f220862129b30f2e8ea955f5a4f5c20ee8a
Valeria Espinoza <76666534+ValDLaw@users.noreply.github.com>
2023-06-05 20:36:51 -0500

    Login fixes + agregar fixes

--- frontend/src/views/Agregar.vue
+++ frontend/src/views/Agregar.vue
@@ -1,27 +1,45 @@
 <template>
-    <div>
+    <div class="shopping-cart">
       <h2>Shopping Cart</h2>
       <div v-if="products.length === 0">
         <p>No products in the cart.</p>
       </div>
       <ul v-else>
-        <li v-for="(product, index) in products" :key="product.id">
-          {{ product.name }} - ${{ product.price }} - Quantity:
-          <input type="number" v-model.number="product.quantity" min="1" @input="calculateTotal(product)">
-          Total: ${{ product.totalPrice }}
-          <button @click="removeProduct(index)">Remove</button>
+        <li v-for="(product, index) in products" :key="product.id" class="cart-item">
+          <div class="product-info">
+            <span class="product-name">{{ product.name }}</span>
+            <span class="product-price">${{ product.price }}</span>
+          </div>
+          <div class="quantity">
+            Quantity:
+            <input type="number" v-model.number="product.quantity" min="1" @input="calculateTotal(product)">
+          </div>
+          <div class="total-price">
+            Total: S/.{{ product.totalPrice }}
+          </div>
+          <button class="remove-btn" @click="removeProduct(index)">Remove</button>
         </li>
-        <p><strong>Overall Total: ${{ overallTotal }}</strong></p>
+        <p class="overall-total"><strong>Overall Total: S/.{{ overallTotal }}</strong></p>
+        <button class="buy-btn" @click="buyProducts">Buy</button>
       </ul>
       <div>
         <h3>Available Products</h3>
         <ul>
-          <li v-for="product in availableProducts" :key="product.id">
-            {{ product.name }} - ${{ product.price }}
-            <input type="number" v-model.number="product.quantityToAdd" min="1">
-            <button @click="addProduct(product)">Add to Cart</button>
+          <li v-for="product in availableProducts" :key="product.id" class="product-item">
+            <div class="product-info">
+              <span class="product-name">{{ product.name }}</span>
+              <span class="product-price">${{ product.price }}</span>
+            </div>
+            <div class="quantity">
+              Quantity:
+              <input type="number" v-model.number="product.quantityToAdd" min="1">
+            </div>
+            <button class="add-btn" @click="addProduct(product)">Add to Cart</button>
           </li>
         </ul>
+      </div>
+      <div v-if="showSuccessMessage" class="success-message">
+        <p>Purchase successful!</p>
       </div>
     </div>
   </template>
@@ -32,11 +50,12 @@
       return {
         products: [],
         availableProducts: [
-          { id: 1, name: 'Inka Kola', price: 2.50, quantityToAdd: 1 },
-          { id: 2, name: 'Coca Cola', price: 2.50, quantityToAdd: 1 },
-          { id: 3, name: 'Fanta', price: 2.0, quantityToAdd: 1 },
-          { id: 3, name: 'Sprite', price: 2.0, quantityToAdd: 1 },
+          { id: 1, name: 'Coca Cola ', price: 2.5, quantityToAdd: 1 },
+          { id: 2, name: 'Inka Kola ', price: 2.5, quantityToAdd: 1 },
+          { id: 3, name: 'Fanta ', price: 2.0, quantityToAdd: 1 },
+          { id: 3, name: 'Sprite ', price: 2.0, quantityToAdd: 1 },
         ],
+        showSuccessMessage: false,
       };
     },
     computed: {
@@ -51,7 +70,7 @@
   
         const quantity = parseInt(quantityToAdd);
         if (!quantity || quantity <= 0) {
-          alert('Please enter a valid quantity.');
+          alert('Por favor, ingres una cantidad valida.');
           return;
         }
   
@@ -75,7 +94,109 @@
       calculateTotal(product) {
         product.totalPrice = product.price * product.quantity;
       },
+      buyProducts() {
+        if (this.products.length === 0) {
+          alert('Please add products to the cart before buying.');
+          return;
+        }
+  
+        // API request, UPDATE DB
+        this.showSuccessMessage = true;
+        this.products = [];
+      },
     },
   };
   </script>
+  
+  <style>
+  .shopping-cart {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+  
+  h2 {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  
+  .cart-item {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+  
+  .product-item {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+  
+  .product-info {
+    display: flex;
+    align-items: center;
+  }
+  
+  .product-name {
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  
+  .quantity {
+    flex: 1;
+    display: flex;
+    align-items: center;
+  }
+  
+  .total-price {
+    flex: 1;
+    text-align: right;
+  }
+  
+  .remove-btn,
+  .add-btn,
+  .buy-btn {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+  
+  .remove-btn {
+    background-color: #dc3545;
+    margin-left: 10px;
+  }
+  
+  .buy-btn {
+    margin-top: 20px;
+    width: 100%;
+  }
+  
+  .success-message {
+    margin-top: 20px;
+    text-align: center;
+    padding: 10px;
+    background-color: #dff0d8;
+    color: #3c763d;
+    border: 1px solid #d6e9c6;
+    border-radius: 5px;
+  }
+  </style>
   
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,31 +2,32 @@
   <div class="container">
     <h1>Acceso usuario</h1>
     <form id="login-form">
-      <p>Correo Institucional</p>
-      <input
-        type="email"
-        placeholder="Ingresa email"
-        v-model="email"
-      >
-      <p>Contraseña</p>
-      <input 
-        type="password"
-        placeholder="Ingresa Contraseña"
-        v-model="password"
-      >
+      <div class="form-group">
+        <label for="email">Correo Institucional</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Ingresa email"
+          v-model="email"
+        >
+      </div>
+      <div class="form-group">
+        <label for="password">Contraseña</label>
+        <input 
+          type="password"
+          id="password"
+          placeholder="Ingresa Contraseña"
+          v-model="password"
+        >
+      </div>
       <div class="submit-button">
         <button type="submit">Login</button>
       </div>
     </form>
-    <br/> <br/>
-
-    <div>
-        <div class>¿No tienes una cuenta?</div>
-        <div class>
-          <router-link to="/register">Regístrate</router-link>
-        </div>
-      </div>
-
+    <div class="signup-link">
+      <p>¿No tienes una cuenta?</p>
+      <router-link to="/register">Regístrate</router-link>
+    </div>
   </div>
 </template>
 
@@ -40,5 +41,66 @@ export default{
     }
   }
 }
-
 </script>
+
+<style>
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+label {
+  font-weight: bold;
+}
+
+input[type="email"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.submit-button {
+  text-align: center;
+  margin-top: 20px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.signup-link {
+  text-align: center;
+  margin-top: 20px;
+}
+
+p {
+  margin-bottom: 10px;
+}
+
+router-link {
+  color: #007bff;
+  text-decoration: underline;
+}
+</style>
